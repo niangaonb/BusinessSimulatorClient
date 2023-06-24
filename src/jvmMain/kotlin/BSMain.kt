25fdca39969bc7047dd91a942309b4b8e857f27e
niangaoa <niangaoa@hotmail.com>
2023-06-24 10:36:38 +0800

    Some small change...

--- src/jvmMain/kotlin/BSMain.kt
+++ src/jvmMain/kotlin/BSMain.kt
@@ -1,11 +1,9 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
